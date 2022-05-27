874fa68998ea96739ecfba4906ca5c91ced95586
Joel <libra-marriaga@hotmail.com>
2022-05-27 13:37:29 -0500

    creando una lista de categorias

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+
 const GifExpertApp = () => {
+
+    /* const categories = ['One Punch', 'Dragon Ball', 'One Piece', 'Naruto', 'Bleach']; */
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'One Piece', 'Naruto', 'Bleach']);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'HunterXHunter']);
+    }
+
     return ( 
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( category => (
+                        
+                        <li key={category}>{category}</li>
+                        
+                    ))
+                }
+
+            </ol>
         </>
     );
 }
